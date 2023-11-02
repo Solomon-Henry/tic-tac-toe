0c8bbf41d1156fcb90b1c09613c5ddbb1e32d570
Solomon-Henry <cherm005@gmail.com>
2023-11-02 16:55:12 -0400

    Tic-Tac-Toe Solomon Henry

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Square (props) {
+  const [value, setValue] = useState(props.input);
+  return <button className='square' onClick={props.onSquareClick}>{props.input}</button>
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Board() {
+  const [squares, setSquares] = useState(Array(9).fill("_"));
+  
+  function handleClick(i) {
+    const remainingSquares = squares.slice();
+    remainingSquares[i] = "X";
+    setSquares(remainingSquares);
+  }  
+
+  return (
+    <>
+      <div className="board-row">
+        <Square input={squares[0]} onSquareClick={() => handleClick(0)}></Square>
+        <Square input={squares[1]} onSquareClick={() => handleClick(1)}></Square>
+        <Square input={squares[2]} onSquareClick={() => handleClick(2)}></Square>
+      </div>
+      <div className="board-row">
+        <Square input={squares[3]} onSquareClick={() => handleClick(3)}></Square>
+        <Square input={squares[4]} onSquareClick={() => handleClick(4)}></Square>
+        <Square input={squares[5]} onSquareClick={() => handleClick(5)}></Square>
+      </div>
+      <div className="board-row">
+        <Square input={squares[6]} onSquareClick={() => handleClick(6)}></Square>
+        <Square input={squares[7]} onSquareClick={() => handleClick(7)}></Square>
+        <Square input={squares[8]} onSquareClick={() => handleClick(8)}></Square>
+      </div>
+    </>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Board />);
